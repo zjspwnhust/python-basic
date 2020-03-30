7153709693438f01712aeed2540e158dc15cc7bd
zjs <329230335@qq.com>
2020-03-30 12:48:34 +0800

    将函数存储在模块中
    Co-Authored-By: Zach Shepherd <zjs@users.noreply.github.com>

---
+++ Fuction/Module.py
@@ -0,0 +1,14 @@
+#导入整个模块
+import pizza
+
+#使用as给导入模块起别名
+import pizza as p
+
+#导入特定的函数
+from pizza import make_pizza
+
+#使用as给函数指定别名
+from pizza import make_pizza as mp
+
+#导入模块中的所有函数
+from pizza import *
